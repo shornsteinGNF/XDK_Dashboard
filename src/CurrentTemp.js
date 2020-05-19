16fcf445d80628942c623828fc51f7083a4c6dde
Hornstein <HORNSSA1@novartis.net>
2020-05-19 16:18:35 -0700

    initial commit

---
+++ src/CurrentTemp.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import {
+	LineChart,
+	CartesianGrid,
+	XAxis,
+	YAxis,
+	Tooltip,
+	Legend,
+	Line,
+	ComposedChart,
+	Area,
+	Bar
+  } from "recharts";
+class TempGraph extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoading: true,
+			dataSource: null
+		};
+	}
+	async componentDidMount() {
+		try {
+			// const response = await fetch('https://jo3lmalso6.execute-api.us-west-1.amazonaws.com/prod');
+			const response = await fetch('https://mt53r15ong.execute-api.us-west-1.amazonaws.com/prod');
+			let responseJson = await response.json();
+			this.setState(
+				{
+					isLoading: false,
+					dataSource: responseJson
+				},
+				function() {}
+			);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	render() {
+		if (this.state.isLoading) {
+			return <div>Loading...</div>;
+		} else {
+			let { dataSource } = this.state;
+			let items = dataSource.body.Items
+			let data = items.map(a => a);
+			const tempData = data.map(row => ({time: row.TimeStamp, temp: row.data.temp}))
+			console.log(tempData)
+			return (
+				<div>
+                    Current Temp: {tempData[tempData.length-1].temp}
+				</div>
+			);
+		}
+	}
+}
+export default TempGraph;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ChartTemp from './ChartTemp'
+import CurrentTemp from './CurrentTemp'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <ChartTemp />
+    <CurrentTemp />
   </React.StrictMode>,
   document.getElementById('root')
 );
