82250f94f693ae742835b7e21d3e4c3935f91e0b
Hornstein <HORNSSA1@novartis.net>
2020-05-22 14:07:48 -0700

    Updates

---
+++ precache-manifest.3e8c761657c8f29fe1d80d3525e6b20a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea3d82bb37e984c905fbeeaad7a835e",
+    "url": "/XDK_React_App/index.html"
+  },
+  {
+    "revision": "0484be5e1e774ebaed87",
+    "url": "/XDK_React_App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "778b6936989e066ad154",
+    "url": "/XDK_React_App/static/js/2.c052bfbe.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/XDK_React_App/static/js/2.c052bfbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0484be5e1e774ebaed87",
+    "url": "/XDK_React_App/static/js/main.2a4ee553.chunk.js"
+  },
+  {
+    "revision": "4151e0b446f58aa047b4",
+    "url": "/XDK_React_App/static/js/runtime-main.87a96a07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/XDK_React_App/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/XDK_React_App/precache-manifest.3e8c761657c8f29fe1d80d3525e6b20a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/XDK_React_App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
