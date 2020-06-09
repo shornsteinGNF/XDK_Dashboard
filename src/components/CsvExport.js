6fe12e2e1c584630ed322c6c2f52a0dafd7c142b
Hornstein <HORNSSA1@novartis.net>
2020-06-09 13:58:00 -0700

    Split up date and time columns in csv export

--- src/components/CsvExport.js
+++ src/components/CsvExport.js
@@ -9,8 +9,55 @@ export class CsvExport extends Component {
               row.TimeStamp > this.props.timeLimit
           );
 
+          function dateConverter(UNIX_timestamp) {
+            var a = new Date(UNIX_timestamp * 1);
+            var months = [
+              "Jan",
+              "Feb",
+              "Mar",
+              "Apr",
+              "May",
+              "Jun",
+              "Jul",
+              "Aug",
+              "Sep",
+              "Oct",
+              "Nov",
+              "Dec",
+            ];
+            var year = a.getFullYear();
+            var month = months[a.getMonth()];
+            var day = a.getDate();
+            if (day.toString().length === 1) {
+              date = "0" + date;
+            }
+            var date =
+              day + " " + month + " " + year;
+            return date;
+          }
+
+          function timeConverter(UNIX_timestamp) {
+            var a = new Date(UNIX_timestamp * 1);
+            var hour = a.getHours();
+            var min = a.getMinutes();
+            var sec = a.getSeconds();
+            if (hour.toString().length === 1) {
+              hour = "0" + hour;
+            }
+            if (min.toString().length === 1) {
+              min = "0" + min;
+            }
+            if (sec.toString().length === 1) {
+              sec = "0" + sec;
+            }
+            var time =
+              hour + ":" + min + ":" + sec;
+            return time;
+          }
+
         const arr = data_filtered.map((row) => ({
-            TimeStamp: row.TimeStamp,
+            Date: dateConverter(row.TimeStamp),
+            Time: timeConverter(row.TimeStamp),
             temp: row.data.temp,
             humidity: row.data.humidity,
             accel_x: row.data.accel_x,
@@ -19,17 +66,20 @@ export class CsvExport extends Component {
             battery: row.data.battery,
           }));
 
-        // var columns = ['TimeStamp', 'Temperature', "Humidity", 'Acceleration (x)', 'Acceleration (y)', 'Acceleration (z)', 'Battery']
+        var headers = ['Date','Time', 'Temperature (C)', "Humidity (%)", 'Acceleration_x', 'Acceleration_y', 'Acceleration_z', 'Battery (%)']
         var res = arr.map(function(item) {
             return Object.values(item);
           });
 
+        res.unshift(headers);
+          
         let csvContent = "data:text/csv;charset=utf-8," + res.map(e => e.join(",")).join("\n");
         var encodedUri = encodeURI(csvContent);
         var link = document.createElement("a");
         link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "my_data.csv");
+        link.setAttribute("download", "xdk_data_"+Date.now()+".csv");
         document.body.appendChild(link); // Required for FF
+        // console.log(res)
         return link.click(); // This will download the data file named "my_data.csv".
     }
     
